a9382bdcb691f67f97889057f937f446e96b3049
3dw1nm0535 <workockmoses@gmail.com>
2017-04-07 18:23:47 +0300

    Final Publish

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,6 +1,10 @@
-var app = angular.module("weather", []);
-app.controller("weatherController", function weatherController($scope, $http) {
- $http.get({
-  
- });
+var app = angular.module('directive', []);
+app.controller('directiveController', ['$scope', function($scope) {
+ $scope.message = 'This is the Custom directive message';
+}]);
+
+app.directive('myDirectiveMessage', function() {
+ return {
+  templateUrl: 'my-directive-message.html';
+ };
 });
